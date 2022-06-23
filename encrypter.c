e652fff4ad3bf547669679eedd087217f3ef4909
Daniel Desmond <df.dezmond9@gmail.com>
2022-06-23 10:39:05 -0400

    now have the basic dict set up w code to show it

--- encrypter.c
+++ encrypter.c
@@ -1,5 +1,12 @@
 #include "encrypter.h"
 
+char* dictionary[26][4] = {"aay", "bee", "see", "dee", "eee", "eff", "gee", "hct", "aye", "jay", "kay", "ell", "emm", "enn", "ohh", "pee", "que", "are", "ess", "tee", "you", "vee", "dub", "exx", "why", "zee"};
+
 int main() {
     printf("Hello, World!\n");
+    for (int i=0; i<26; i++) {
+        printf("%s\n", dictionary[0][i]);
+    }
 }
+
+
