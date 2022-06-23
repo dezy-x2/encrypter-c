b8dec8d30791b75f747347eb1c9b65e5809c33c8
Daniel Desmond <df.dezmond9@gmail.com>
2022-06-23 11:07:36 -0400

    encrypter currently works go to README for more

--- encrypter.c
+++ encrypter.c
@@ -7,8 +7,22 @@ int main() {
     for (int i=0; i<26; i++) {
         printf("%s\n", dictionary[0][i]);
     }
+
+    char* encrypted = encrypt("danieldesmond");
+    printf("%s\n", encrypted);
 }
 
 int getCharCode(char c) {
     return (int)c - 97;
+}
+
+char* encrypt(char* str) {
+    char* finalStr = (char*)calloc(1, sizeof(3 * strlen(str)));
+
+    for (int i=0; i<strlen(str); i++) {
+        int replacementIdx = getCharCode(str[i]);
+        finalStr = strncat(finalStr, dictionary[0][replacementIdx], sizeof(finalStr) + sizeof(dictionary[0][replacementIdx]));
+    }
+
+    return finalStr;
 }
