661d19cbe65e368367b6422e4446885a32486092
Daniel Desmond <df.dezmond9@gmail.com>
2022-06-25 10:06:30 -0700

    fixed the error & it was the smallest thing

--- encrypter.c
+++ encrypter.c
@@ -49,28 +49,14 @@ char* encrypt(char* str) {
  */
 char** generateKey() {
     srand(time(NULL));
-    char** final = (char**)calloc(1, sizeof(26 * 4 * sizeof(char*)));
+    char** final = (char**)calloc(1, 26 * 4 * sizeof(char*));
     for (int i=0; i<26; i++) {
-        // printf("\t%d\n", i);
         char* tempStr = (char*)calloc(1, 4*sizeof(char));
         for (int j=0; j<3; j++) {
-            // printf("\t\t%d\n", j);
             char c = (char)(rand() % 26) + 97;
-            // printf("\t\tln: 39\n");
             tempStr = strncat(tempStr, &c, sizeof(c) + sizeof(tempStr));
-            // printf("%d: %s\n", i, tempStr);
-            // printf("\t\tln: 41\n");
         }
-        // printf("\tln: 43\n");
         final[i] = tempStr;
-        printf("%d: %s\n", i, final[i]);
-        // printf("\tln: 45\n");
     }
-    printf("!!!!!!!!!!!%s\n", final[0]);
-    printf("===================================\n");
-    for (int i=0; i<26; i++) {
-        printf("%d: %s\n", i, final[i]);
-    }
-    printf("===================================\n");
     return final;
 }
