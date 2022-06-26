cb7eaab787f83718874a642d564f3d73b145f8ef
Daniel Desmond <df.dezmond9@gmail.com>
2022-06-25 18:39:54 -0700

    small adjustements

--- encrypter.c
+++ encrypter.c
@@ -8,13 +8,13 @@ int main() {
     //     printf("%s\n", dictionary[0][i]);
     // }
 
-    // char* encrypted = encrypt("danieldesmond");
-    // printf("%s\n", encrypted);
+    char* encrypted = encrypt("danieldesmond");
+    printf("%s\n", encrypted);
 
-    char** newDict = generateKey();
-    for (int j=0; j<26; j++) {
-        printf("%d: %s\n",j, newDict[j]);
-    }
+    // char** newDict = generateKey();
+    // for (int j=0; j<26; j++) {
+    //     printf("%d: %s\n",j, newDict[j]);
+    // }
 }
 
 /**
@@ -34,7 +34,7 @@ int getCharCode(char c) {
  * @return char* - the final encrypted string
  */
 char* encrypt(char* str) {
-    char* finalStr = (char*)calloc(1, sizeof(3 * strlen(str)));
+    char* finalStr = (char*)calloc(1, sizeof(char) * 3 * strlen(str));
     for (int i=0; i<strlen(str); i++) {
         int replacementIdx = getCharCode(str[i]);
         finalStr = strncat(finalStr, dictionary[0][replacementIdx], sizeof(finalStr) + sizeof(dictionary[0][replacementIdx]));
@@ -49,9 +49,9 @@ char* encrypt(char* str) {
  */
 char** generateKey() {
     srand(time(NULL));
-    char** final = (char**)calloc(1, 26 * 4 * sizeof(char*));
+    char** final = (char**)calloc(1, 26 * sizeof(char*));
     for (int i=0; i<26; i++) {
-        char* tempStr = (char*)calloc(1, 4*sizeof(char));
+        char* tempStr = (char*)calloc(1, 4 * sizeof(char));
         for (int j=0; j<3; j++) {
             char c = (char)(rand() % 26) + 97;
             tempStr = strncat(tempStr, &c, sizeof(c) + sizeof(tempStr));
